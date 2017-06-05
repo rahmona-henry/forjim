ce1d8e0fd2c92197e3014cc763045e92eac69a78
rahmona-henry <rahmona.henry@icloud.com>
2017-06-05 17:03:48 +1200

    homepage text in place

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('client'))
+
 app.get('/',function(req,res){
-  res.send('Hello Lower Shotover')
+  res.send(index.html)
 })
 
 app.listen(3000, function(){
